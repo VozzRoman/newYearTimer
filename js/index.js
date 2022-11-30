66c6f6032405c77c33c331d9594fc37bc3a59bfa
VozzRoman <looper0500@ukr.net>
2022-11-30 18:40:54 +0200

    adding

--- js/index.js
+++ js/index.js
@@ -1,29 +1,68 @@
 
-const finishTime = new Date('2023-01-01 00:00');
-console.log(finishTime);
 
-const minutes = 1000 * 60;
-const hours = minutes * 60;
-const days = hours * 24;
+class NewYearTimer {
 
+	constructor({ selector, targetData }) {
+		this.selector = selector;
+		this.targetData = targetData;
+	}
 
-
-setInterval(() => {
-    const startTime = new Date();
+	startTime() {
+		setInterval(() => {
+		this.getTimeDate();
+}, 1000)
+}
+	getTimeDate() {
+	const finishTime = new Date(`${this.targetData}`);
+	console.log(finishTime);
+	const startTime = new Date();
     console.log(startTime);
     const differenceTime = finishTime - startTime;
-    console.log(differenceTime);
-    const daysToNewYear = Math.floor(differenceTime / days)
+	console.log(differenceTime);
+	this.convertTime(differenceTime);
+ 
+	}
+
+	convertTime(stampTime) {
+	const minutes = 1000 * 60;
+	const hours = minutes * 60;
+	const days = hours * 24;
+	const daysToNewYear = Math.floor(stampTime / days)
     console.log(daysToNewYear);
-    const hoursToNewYear = Math.floor((differenceTime % days) / hours);
+    const hoursToNewYear = Math.floor((stampTime % days) / hours);
     console.log(hoursToNewYear);
-    const minutesToNewYear = Math.floor((differenceTime % hours) / minutes);
-    console.log(minutesToNewYear);
+    const minutesToNewYear = Math.floor((stampTime % hours) / minutes);
+	console.log(minutesToNewYear);
+	const secondsToNewYear = Math.floor((stampTime % minutes) / 1000);
+	console.log(secondsToNewYear);
+	this.insertDataTime(daysToNewYear, hoursToNewYear, minutesToNewYear, secondsToNewYear);
+	}
 
-}, 1000)
+	insertDataTime(days, hours, minutes, seconds) {
+const daysRefs = document.querySelector(`${this.selector} [data-value="days"]`);
+console.log(daysRefs);
+const hoursRefs = document.querySelector(`${this.selector} [data-value="hours"]`);
+console.log(hoursRefs);
+const minutesRefs = document.querySelector(`${this.selector} [data-value="minutes"]`);
+console.log(minutesRefs);
+const secondsRefs = document.querySelector(`${this.selector} [data-value="seconds"]`);
+	console.log(secondsRefs);
+	daysRefs.textContent = days.toString().padStart(2, '0');
+	hoursRefs.textContent = hours.toString().padStart(2, '0');
+	minutesRefs.textContent = minutes.toString().padStart(2, '0');
+	secondsRefs.textContent = seconds.toString().padStart(2, '0');
+}
+
+}
 
 
 
+const startNy24 = new NewYearTimer({
+	selector: '#clock1',
+	targetData: '2023-01-01 00:00',
+})
+
+startNy24.startTime();
 
 
 //-----
